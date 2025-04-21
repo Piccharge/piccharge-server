cc938bfd214b54a5e463d93c9f23eae8a9371d45
dgh06175 <dgh06175@gmail.com>
2025-04-21 21:28:26 +0900

    init spring project

---
+++ src/main/java/com/pohyoja/picchargeserver/PicchargeServerApplication.java
@@ -0,0 +1,12 @@
+package com.pohyoja.picchargeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicchargeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicchargeServerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/pohyoja/picchargeserver/PicchargeServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.pohyoja.picchargeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicchargeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
