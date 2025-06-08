1d9d5f7074a8d7629defee451ceee379a12783dc
dgh06175 <dgh06175@gmail.com>
2025-06-08 20:35:14 +0900

    feat: photo 테이블에 복합키 추가, 쿼리 성능 개선 위함

--- src/main/java/com/pohyoja/picchargeserver/domain/photo/entity/Photo.java
+++ src/main/java/com/pohyoja/picchargeserver/domain/photo/entity/Photo.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
